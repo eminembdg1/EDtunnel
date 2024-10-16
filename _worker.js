10e62f4e7cf9e8d02c7b44313a0b51b3b6fd014a
eminembdg1 <eminembdg@outlook.com>
2024-10-16 17:12:19 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'bd49b12d-fd56-4313-a437-749d6e629a83';
 
 const proxyIPs = ['193.239.166.244'];
 
